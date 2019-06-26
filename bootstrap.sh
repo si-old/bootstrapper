571bdcb9677e82491b1cabeb5def480ff9c05cfb
Giuseppe Masino <dev.giuseppemasino@gmail.com>
2019-06-26 19:22:25 +0200

    Full rewrite

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,11 +25,26 @@ askPrompt()
 		read "$1"
 	}
 
+checkExecutable()
+	{
+		if ! command -v "$1" > /dev/null ; then
+				echo "ERROR: $1 executable not found in PATH"
+				exit 2
+			fi
+	}
+
+abort()
+	{
+		echo "[*] Error. Program aborted. Cleaning up"
+		eval 'rm -rf $projectName'
+		exit $1
+	}
+
 scriptName="$( echo "$0" | sed s/^.*\\/// )"
 scriptDir="$( echo "$0" | sed s/[\\/]"${scriptName}"// )"
 
 echo
-echo "${scriptName} : StudentIngegneria IT Committee project bootstrapper"
+echo "${scriptName} : StudentIngegneria IT Committee git flow bootstrapper"
 echo
 echo "====================================================================="
 echo "Copyright (C) 2019  StudentIngegneria"
@@ -47,34 +62,76 @@ echo "You should have received a copy of the GNU General Public License"
 echo "along with this program.  If not, see <https://www.gnu.org/licenses/>."
 echo "======================================================================"
 echo
-echo "WARNING: THIS SCRIPT IS DESTRUCTIVE, use only to create new projects"
 echo
 
 # Check preconditions
 
-if ! command -v git > /dev/null ; then
-	echo "ERROR: Git executable not found in PATH"
-	exit 2
-fi
+checkExecutable git
+checkExecutable git-flow
 
-askPrompt projectPath "Project path ( last element will be the project name )"
+askPrompt projectPath   "Project path ( last element will be the project name )"
+askPrompt useOnlineRepo "Sync with an existing online repo ? [yn]"
 
 # shellcheck disable=SC2154
 projectName="$( echo "${projectPath}" | sed s/^.*\\/// )"
 
-echo
-askPrompt projectIsPublic "Will this be uploaded to GitHub ? [yn]"
+# TODO: Fix paths if . is specified as project path
 
-# Create project
+# Setup project
 
 mkdir -p "$projectPath" || exit $?
-cp -rf "${scriptDir}/gitdir" "${projectPath}/.git" || exit $?
+cd "$projectPath"       || exit $?
 
-cd "$projectPath" || exit $?
+git init
+
+echo ":: Configuring git flow"
+
+git config --local --add gitflow.branch.master     release
+git config --local --add gitflow.branch.develop    dev
+git config --local --add gitflow.prefix.feature    feature/
+git config --local --add gitflow.prefix.bugfix     bugfix/
+git config --local --add gitflow.prefix.release    rc/
+git config --local --add gitflow.prefix.hotfix     hotfix/
+git config --local --add gitflow.prefix.support    support/
+git config --local --add gitflow.prefix.versiontag v
+git config --local --add gitflow.path.hooks        .git/hooks
+
+echo ":: Configuring remotes"
+
+while git remote | grep -E '^si$' > /dev/null ; do
+
+		echo -e "\t/!\\ WARNING: \"si\" remote already exists. Delete or rename it"
+		echo -e "\t/!\\ Dropping a subshell. Invoke 'exit' when you're done"
+
+		$SHELL
+
+		echo -e "\t:: Resuming script execution\n"
+
+	done
+
+git remote add si "https://github.com/StudentIngegneria/${projectName}"
 
 # shellcheck disable=SC2154
-if [ "$projectIsPublic" = "y" ] ; then
-	git remote set-url si "https://github.com/StudentIngegneria/${projectName}"
-fi
+if [ "$useOnlineRepo" = "y" ] ; then
 
-echo DONE
+		echo ":: Synchronizing with remote repository"
+		git fetch --all -q || abort $?
+
+		echo ":: Setting up branches"
+		git checkout -q dev
+
+	else
+
+		echo ":: Bootstrapping branches"
+		git checkout -b dev
+
+		# If log is empty
+		if [ "$( git log > /dev/null ; echo $? )" -eq 128 ] ; then
+
+				echo ":: Creating initial commit"
+				git commit --allow-empty --quiet -m "Initial commit"
+			fi
+
+	fi
+
+echo ":: DONE"
